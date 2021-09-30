9bad98e25e401dd19f14da0ee300e7d0f77dbf50
Sam Kilpatrick <samkilpatrick903@gmail.com>
2021-09-30 09:54:01 -0500

    Added in bootstrap grid

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,9 @@
-var today = function(){
-    var day = moment();
-    $("#time").text(day.format("dddd, MMMM Do YYYY, hh:mm:ss a"));
-    }
-    setInterval(today, 1000);
+// var today = function(){
+//     var day = moment();
+//     $("#time").text(day.format("dddd, MMMM Do YYYY, hh:mm:ss a"));
+//     }
+//     setInterval(today, 1000);
+
+// Try new function to show current date
+
+$("#currentDay").text(moment.format("dddd, MMMM Do, YYYY"));
