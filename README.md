5944fafa2f37c913053c813cc9d623b2c66bcdc8
Sam Kilpatrick <samkilpatrick903@gmail.com>
2021-10-05 18:58:39 -0500

    Updated readme and JS

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Assignment
 Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
 You'll need to use the Moment.js library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
+
+Credit
+Classmates and tutoring helped with functions
+
+Screenshot
+![Screenshot](./assets//Users/samkilpatrick/Desktop/UT-Bootcamp/Homework/SK_Homework_5/assets/screencapture-127-0-0-1-5502-index-html-2021-10-05-18_53_32.png)
--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,30 @@
-// var today = function(){
-//     var day = moment();
-//     $("#time").text(day.format("dddd, MMMM Do YYYY, hh:mm:ss a"));
-//     }
-//     setInterval(today, 1000);
-
-// Try new function to show current date
 
 $("#currentDay").text(moment.format("dddd, MMMM Do, YYYY"));
+
+var moment = moment();
+var currentHour = moment.hour();
+var saveBtn = $(".saveBtn");
+var plannedEvent = $("#eventInput");
+var userInput;
+var localInput = localStorage.getItem("plannedEvent");
+var keyName;
+
+$(".saveBtn").on("click", saveEvent);
+
+function saveEvent(event) {  
+    var userEvent = $(this).siblings("input").val();
+    console.log(userEvent);
+    var keyName = $(this).attr("id");
+    console.log(keyName);
+    localStorage.setItem(keyName, userEvent);
+};
+
+$(document).ready(function(){ 
+    $("button").each(function(){
+        for (var i = 9; i <= 17; i++){
+        $("#"+i).siblings("input").val(localStorage.getItem(""+i))
+        }
+    })
+});
+
+// Need to adjust timeblocks
